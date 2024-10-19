347dbcd4caac2953a98b9ac4e88dabd5eee3005f
Pappu Yadav <yadavpappu3661@gmail.com>
2024-10-19 22:25:11 +0530

    signup link and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
-import SignIn from './Pages/Signin';
-import SignUp from './Pages/Signup';
+import SignIn from './Pages/SignIn';
 import Dashboard from './Pages/Dashboard';
 import Projects from './Pages/Projects';
 import Header from './components/Header';
-
+import Footer from './components/Footer';
+import SignUp from './Pages/Signup';
 
 export default function App() {
   return (
-  
     <BrowserRouter>
       <Header />
-      
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/project' element={<Projects />} />
-        
-        </Routes>
-  
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
-
