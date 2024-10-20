5be5a3e3c9a31a1dd7cdbe090e7a2aa21adfa6a4
Pappu Yadav <yadavpappu3661@gmail.com>
2024-10-20 14:10:08 +0530

    create api route with edit functionality in post

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, signout, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, getUsers, signout, test, updateUser } from '../controllers/user.controller.js';
 
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -10,5 +10,5 @@ router.put('/update/:userId', verifyToken,updateUser);
 router.delete('/delete/:userId', verifyToken,deleteUser);
 router.post('/signout', signout);
 
-
+router.get('/getusers', verifyToken, getUsers);
 export default router;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import SignUp from './Pages/Signup';
 import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './Pages/CreatePost';
+import UpdatePost from './Pages/UpdatePost';
+
 
 
 
@@ -27,6 +29,7 @@ export default function App() {
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
         </Route>
 
         <Route path='/projects' element={<Projects />} />
