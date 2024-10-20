0b3bc4d7b3b0b721a4e19714e385116c04dd91c4
Pappu Yadav <yadavpappu3661@gmail.com>
2024-10-20 18:24:44 +0530

    add admin dashboard

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPost';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -30,6 +31,8 @@ export default function Dashboard() {
       {tab === 'users' && <DashUsers />}
         {/* comments  */}
         {tab === 'comments' && <DashComments />}
+         {/* dashboard comp */}
+      {tab === 'dash' && <DashboardComp />}
     </div>
   );
 }
